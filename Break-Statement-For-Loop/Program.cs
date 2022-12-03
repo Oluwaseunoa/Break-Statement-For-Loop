a6446d1c7b436e1ff14d3b19a04a2be8a9880d95
oluwaseunoa <oluwaseun.beicks@gmail.com>
2022-12-03 16:20:06 +0100

    Add project files.

---
+++ Break-Statement-For-Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+        int pass = 5;
+        //for loop: 0-9
+        for (int i = 0; i < 10; i++)
+        {
+            if(i == pass)
+            {
+                break;
+            }
+            System.Console.WriteLine(i);
+        }
+        System.Console.ReadKey();
+    }
+}
